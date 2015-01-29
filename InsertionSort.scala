90624afc1070b38888a54ba1907bcf2be988587a
Jorge Arturo Xicali Flores <jorgearturox01@hotmail.com>
2015-01-28 18:55:34 -0600

    Entrego el algoritmo de Ordenamiento por Insercion

--- InsertionSort.scala
+++ InsertionSort.scala
@@ -1,23 +1,28 @@
-class InsertionSort{
-  def sort(A: Array[Int], asc: Boolean){
-    var key = 0
-    var i = 0
+object Insertion{
 
-    for (j <- 1 to A.length - 1) {
-      key = A(j)
-      i = j - 1
-      while (i >= 0 && compare(A(i), key, asc)) {
-        A(i + 1) = A(i)
-        i = i - 1
-      }
-      A(i + 1) = key
-    }
-  }
+def main(args:Array[String]){
 
-  def compare(a: Int, b: Int, asc: Boolean): Boolean = {
-    if (true == asc) {
-      return a > b
-    }
-    return a <= b
-  }
+var A=Array(6,5,3,1,8,7,2,4)
+
+var j=0
+var temp=0
+
+for(i<-1 to 7) {
+j=i
+while( j > 0 && A(j-1) > A(j) ) {
+temp =A(j)
+A(j)=A(j-1)
+A(j-1)=temp
+j=j-1
+}
+}
+println("El vector es:")
+var i=0
+
+while(i<8){
+print(A(i)+" ")
+i=i+1
+}
+
+}
 }
